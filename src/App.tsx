f10534212a667c1296945afa101392bbda24ba58
CodedSakura <sakuracoded@gmail.com>
2023-10-02 08:50:06 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.scss'
+
+function App() {
+  return <>
+    <div class="grid--full-width">nav</div>
+    <div>side</div>
+    <div>main</div>
+    <div>q</div>
+    <div class="grid--full-width">bottom</div>
+  </>;
+}
+
+export default App
